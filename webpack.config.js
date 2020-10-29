4209b351d0cdfe9173177f981370031d76d962f7
Lior <liorha@gmail.com>
2020-10-29 12:24:30 +0200

    setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,54 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // Entry -> Output
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new MiniCssExtractPlugin();
 
-    // loader
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-        ]
-    },
-    
-    devtool: 'eval-cheap-module-source-map',// Mapping the files to show in the console log instead of just bundle.js
+    return ({
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        plugins: [
+            CSSExtract          
+        ],
 
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true // Returns this same page for all 404 routes
-    }
+        // loader
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader'
+                    }
+                },
+                {
+                    test: /\.s?css$/,
+                    use: [MiniCssExtractPlugin.loader, {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }, {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }],
+                },
+            ]
+        },
+        
+        devtool: isProduction ? 'source-map' : 'inline-source-map',// Mapping the files to show in the console log instead of just bundle.js
+
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true // Returns this same page for all 404 routes
+        }
+    })
 };
